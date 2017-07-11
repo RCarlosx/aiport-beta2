7a25ce4f3cdb6b244ff7d7e42a541e37f6183b30
RCarlosx <carlosrsrs7@gmail.com>
2017-07-11 17:00:23 -0500

    Add files via upload

---
+++ airport-demo/canvas-main.js
@@ -0,0 +1,19 @@
+$gmx(document).ready(function() {
+   //var canvas_main = $('canvas.a-canvas canvas.a-grab-cursor').height();
+   var cnv = document.getElementsByTagName("canvas")[0];//.style.height="300px";
+   cnv.style.height = "300px";
+   //document.getElementById("id").style.property="value"
+   //alert(cnv);
+   console.log(cnv[0]);
+
+   
+});
+
+/*$(document).ready(function(){
+   //var cnv = $('canvas').html();
+   var cnv = document.getElementsByTagName("canvas");
+   alert(cnv);
+});*/
+
+//var cnv = $('canvas.a-canvas').html();
+//alert(cnv);
